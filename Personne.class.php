89af3f17e2ba15763ba83e8aa16ba1cd5f68667f
Boudjadjasamira <67705912+Boudjadjasamira@users.noreply.github.com>
2020-07-30 14:47:15 +0200

    Class Personne /Employé/Client

---
+++ Personne.class.php
@@ -0,0 +1,160 @@
+<?php
+    class Personne{
+
+    //Déclaration des attributs
+        protected $nom;
+        protected $prenom;
+        protected $dateDeNaissance;
+        protected $email;
+        protected $tel;
+        protected $adresse;
+
+    //Déclaration du constructeur
+    public function __construct($nouveauNom,$nouveauPrenom,$nouveauDateDeNaissance,$nouveauEmail,$nouveauTel,$nouveauAdresse ){
+        $this->nom = $nouveauNom;
+        $this->prenom = $nouveauPrenom;
+        $this->dateDeNaissance = $nouveauDateDeNaissance;
+        $this->email = $nouveauEmail;
+        $this->tel = $nouveauTel;
+        $this->adresse = $nouveauAdresse;
+    }
+
+
+    //Getter & Setter
+        /**
+         * @return mixed
+         */
+        public function getNom()
+        {
+            return $this->nom;
+        }
+
+        /**
+         * @param mixed $nom
+         */
+        public function setNom($nom): void
+        {
+            $this->nom = $nom;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getPrenom()
+        {
+            return $this->prenom;
+        }
+
+        /**
+         * @param mixed $prenom
+         */
+        public function setPrenom($prenom): void
+        {
+            $this->prenom = $prenom;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDateDeNaissance()
+        {
+            return $this->dateDeNaissance;
+        }
+
+        /**
+         * @param mixed $dateDeNaissance
+         */
+        public function setDateDeNaissance($dateDeNaissance): void
+        {
+            $this->dateDeNaissance = $dateDeNaissance;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getEmail()
+        {
+            return $this->email;
+        }
+
+        /**
+         * @param mixed $email
+         */
+        public function setEmail($email): void
+        {
+            $this->email = $email;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getTel()
+        {
+            return $this->tel;
+        }
+
+        /**
+         * @param mixed $tel
+         */
+        public function setTel($tel): void
+        {
+            $this->tel = $tel;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getAdresse()
+        {
+            return $this->adresse;
+        }
+
+        /**
+         * @param mixed $adresse
+         */
+        public function setAdresse($adresse): void
+        {
+            $this->adresse = $adresse;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
