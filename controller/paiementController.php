e97a5a87ef6cfa6632ff733dfcbf8b7c0390ef11
extraLara <lecomtelara@hotmail.fr>
2020-08-14 15:34:27 +0200

    Verifie si l'utilisateur est connecte ou pas avant de reserver

--- controller/paiementController.php
+++ controller/paiementController.php
@@ -3,12 +3,12 @@ session_start();
 //Récupération des variables
 $idChambreRecup = $_POST['idChambre'];
 $prixChambreRecup = $_POST['prixChambre'];
+
 //Voir si connecte
-/*if($_SESSION['logged'] == 1){
+if($_SESSION['$2y$10$5yV9XVOkQPowxCuywSdSMOO3ciGZYwfl3YkoRSMiFlUCdJcM93UIS'] == 1){
     header('Location: ../view/L&S_HOTEL/paiement.php?id='.$idChambreRecup);
 }else{
     header('Location: ../view/L&S_HOTEL/connexion.php');
-}*/
+}
 
-header('Location: ../view/L&S_HOTEL/paiement.php?id='.$idChambreRecup);
 ?>
