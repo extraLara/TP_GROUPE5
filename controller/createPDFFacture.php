c914115013dd076d45b189c9a62d4779ade355b9
Lecomte Lara <lecomtelara@hotmail.fr>
2020-08-21 16:26:10 +0200

    nouvelle biblio pdf

--- controller/createPDFFacture.php
+++ controller/createPDFFacture.php
@@ -1,92 +1,157 @@
 <?php
 session_start();
-error_reporting(0);
-require_once('../classe/tcpdf/tcpdf.php');
+// include autoloader
+require_once '../classe/dompdf/autoload.inc.php';
 
 //Récupération de l'id chambre
 $recupIDChambre = $_GET['idChambre'];
 
-$recupPaiement = array();
-//Importation des lignes
-$handle = fopen("../input/Paiement.csv", "r");
-for ($i = 0;$row = fgetcsv($handle);$i++) {
-    //Tant que j'ai une ligne, j'ajoute dans mon tableau
-    array_push($recupPaiement, $row);
-}
-//Je ferme le fichier
-fclose($handle);
-//Suppression du premier element
-array_shift($recupPaiement);
+$html = '<!DOCTYPE html>
+<html>
 
-$recupReservation = array();
-//Importation des lignes
-$handle = fopen("../input/Reservation.csv", "r");
-for ($i = 0;$row = fgetcsv($handle);$i++) {
-    //Tant que j'ai une ligne, j'ajoute dans mon tableau
-    array_push($recupReservation, $row);
-}
-//Je ferme le fichier
-fclose($handle);
-//Suppression du premier element
-array_shift($recupReservation);
+    <head>
+    <title>Confirmation de reservation - L&S Hotel</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
 
 
-$html = '<table>';
-$html .= '<tr>';
+<!-- css du mail --> 
 
-foreach($recupPaiement as $value){
-    if(explode(';', $value[0])[6] == $recupIDChambre){
-        $html .= '<td colspan="3">Le client : '.explode(";", $value[0])[1].'</td>';
-    }
-  }   
+<link rel="stylesheet" type="text/css" media="screen" href="../view/L&S_HOTEL/pdf/style.css" />
 
-$html .= '</tr>';
+<!-- debut --> 
+<body style="margin: 0 !important; padding: 0 !important; background-color: #eeeeee;" bgcolor="#eeeeee">
 
-$html .= '<tr>';
-$html .= '<td>Nom Chambre</td>';
-$html .= '<td>Prix</td>';
-$html .= '<td>Date</td>';
-$html .= '</tr>';
+<div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: Abel, Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
+</div>
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tr>
+            <td align="center" style="background-color: #eeeeee;" bgcolor="#eeeeee">
+                <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+                    <tr>
+                        <td align="center" valign="top" style="font-size:0; padding: 35px; background-color: #082d41;" bgcolor="#082d41">
+                            <div style="display:inline-block; max-width:50%; min-width:100px; vertical-align:top; width:100%;">
+                                <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:300px;">
+                                    <tr>
+                                        <td align="center"> <img src="../view/L&S_HOTEL/pdf/logo.png" width="200" height="200" style="display: block; border: 0px;" /> </td>
+                                    </tr>
+                                    <tr>
+                                </table>
+                            </div>                    
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" style="padding: 35px 35px 20px 35px; background-color: #ffffff;" bgcolor="#ffffff">
+                            <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+                                <tr>
+                                    <td align="center" style="font-family:abel, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding-top: 25px;"><br>
+                                        <h2 style="font-size: 25px; font-weight: 800; line-height: 36px; color: #333333; margin: 0;"> Récapitulatif de votre réservation :! </h2>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" style="font-family: abel, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding-top: 10px;">
+                                        <p style="font-size: 16px; font-weight: 400; line-height: 24px; color: #777777;"> Ci-dessous le récapitulatif de votre reservation à l\'Hôtel L&S Hôtel & SPA. </p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="left" style="padding-top: 20px;">
+                                        <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                                            <tr>
+                                                <td width="75%" align="left" bgcolor="#eeeeee" style="font-family: abel, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;"> Date de reservation : </td>
+                                                <td width="25%" align="left" bgcolor="#eeeeee" style="font-family: abel, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;"> 10/08/2020 </td> <!-- date reservation -->  
+                                            </tr>
+                                            <tr>
+                                                <td width="75%" align="left" style="font-family: abel, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 15px 10px 5px 10px;"> Chambre 1 </td> <!-- changer chambre -->  
+                                                <td width="25%" align="left" style="font-family: abel, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 15px 10px 5px 10px;">200 € </td>  <!-- changer prix  -->  
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="left" style="padding-top: 20px;">
+                                        <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                                            <tr>
+                                                <td width="75%" align="left" style="font-family: abel, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;"> TOTAL </td>
+                                                <td width="25%" align="left" style="font-family: abel, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;"> 200€ </td> <!-- changer prix  -->  
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" height="100%" valign="top" width="100%" style="padding: 0 35px 35px 35px; background-color: #ffffff;" bgcolor="#ffffff">
+                            <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:660px;">
+                                <tr>
+                                    <td align="center" valign="top" style="font-size:0;">
+                                        <div style="display:inline-block; max-width:50%; min-width:240px; vertical-align:top; width:100%;">
+                                            <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:300px;">
+                                                <tr>
+                                                    <td align="left" valign="top" style="font-family: abel, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px;">
+                                                        <p style="font-weight: 800;">Description :</p>
+                                                        <p></p>
 
+                                                        <p style="font-weight: 800;">Options :</p>
+                                                        <p></p>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </div>
 
-$html .= '<tr>';
- 
-foreach($recupPaiement as $value){
-    if(explode(';', $value[0])[6] == $recupIDChambre){
-        $html .= '<td>'.explode(";", $value[0])[5].'</td>';
-        $html .= '<td>'.explode(";", $value[0])[7].' €</td>';
-    }
-  }    
-  foreach($recupReservation as $row){
-    if(explode(';', $row[0])[2] == $recupIDChambre){
-        $html .= '<td>'.explode(";", $row[0])[3].' au '.explode(";", $row[0])[4].'</td>';
-    }
-  }
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" style=" padding: 35px; background-color: #082d41;" bgcolor="#1b9ba3">
+                            <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+                                <tr>
+                                    <td align="center" style="font-family: abel, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding-top: 25px;">
+                                        <h2 style="font-size: 20px; font-weight: 800; line-height: 30px; color: #ffffff; margin: 0;"> Cliquez ci-dessous pour accéder à votre espace client: </h2>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" style="padding: 25px 0 15px 0;">
+                                        <table border="0" cellspacing="0" cellpadding="0">
+                                            <tr>
+                                                <td align="center" style="border-radius: 5px;" bgcolor="#66b3b7"> 
+                                                <a href="#" target="_blank" style="font-size: 14px; font-family: Open Sans, Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; border-radius: 5px; background-color: #eebb4d; padding: 15px 30px; border: 1px solid #eebb4d; display: block;">Mon Espace</a> </td>
+                                            </tr>
+                                            <td align="left" style="font-family: abel, Helvetica, Arial, sans-serif; font-size: 25px; font-weight: 400; line-height: 24px;">
+                                                <br>
+                                                <p style="font-size: 13px; font-weight: 400; line-height: 20px; color: #ffffff63;"> Si vous n\'avez pas créé de compte à l\'aide de cette adresse e-mail, veuillez l\'ignorer ou vous <a href="#" target="_blank" style="color: #77777765;">désinscrire.</a>. </p>
+                                            </td>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
 
- 
+</html>';
 
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
 
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml($html);
+//$dompdf->load_html_file('../view/L&S_HOTEL/pdf/pdf.html');
 
-  $html .= '</tr>';
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
 
-$html .= "</table>";
+// Render the HTML as PDF
+$dompdf->render();
 
-
-// create new PDF document
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-
-// set document information
-$pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('L&S HOTEL');
-$pdf->SetTitle('Facture');
-
-// add a page
-$pdf->AddPage();
-
-
-// output the HTML content
-$pdf->writeHTML($html);
-
-//Close and output PDF document
-$pdf->Output('facture.pdf', 'I'); 
+// Output the generated PDF to Browser
+$dompdf->stream();
 ?>
